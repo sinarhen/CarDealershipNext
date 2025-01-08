15d81c659203b281e2b817073c2041b90a252852
sinarhen <nicklchannel8@gmail.com>
2025-01-08 17:14:13 +0200

    Cleaned up the code

--- app/(other)/result/[makeId]/[year]/components/results.tsx
+++ app/(other)/result/[makeId]/[year]/components/results.tsx
@@ -1,73 +1,76 @@
-import { carsEndpoint } from "@/lib/constants";
-import { getYearsRangingFrom } from "@/lib/utils";
-import { VehicleDetailsResponse, VehicleListResponse } from "@/lib/types";
-import { Separator } from "@/components/ui/separator";
-import { Button } from "@/components/ui/button";
-import { Info } from "lucide-react";
+import {carsEndpoint} from "@/lib/constants";
+import {getYearsRangingFrom} from "@/lib/utils";
+import {VehicleDetailsResponse, VehicleListResponse} from "@/lib/types";
+import {Separator} from "@/components/ui/separator";
+import {Button} from "@/components/ui/button";
+import {Info} from "lucide-react";
 
 interface Params {
-  makeId: string;
-  year: string;
+    makeId: string;
+    year: string;
 }
 
-export default async function Results({ params }: { params: Params }) {
-  const { year, makeId } = await params;
+export default async function Results({params}: { params: Params }) {
+    const {year, makeId} = await params;
 
-  const response = await fetch(
-    `https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeIdYear/makeId/${makeId}/modelyear/${year}?format=json`
-  );
-  const result: VehicleDetailsResponse = await response.json();
-
-  await new Promise((resolve) =>
-    setTimeout(() => {
-      resolve("done");
-    }, 5000)
-  );
-
-  if (!response.ok) {
-    return (
-      <div>
-        <h1>Failed to fetch data</h1>
-        <p>{result.Message}</p>
-      </div>
+    const response = await fetch(
+        `https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeIdYear/makeId/${makeId}/modelyear/${year}?format=json`
     );
-  }
+    const result: VehicleDetailsResponse = await response.json();
 
-  return (
-    <>
-      <h1 className="text-xl">
-        Found {result.Count} {result.Results[0].Make_Name} cars for {year}
-      </h1>
-      <Separator className="mt-4 mb-6" />
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3">
-        {result.Results.map((car) => (
-          <div
-            key={car.Model_ID}
-            className="p-3 hover:border-black/50 h-[400px] flex-col justify-between transition-colors rounded border"
-          >
+
+    // Imitate long loading to use suspense at the parent component
+    await new Promise((resolve) =>
+        setTimeout(() => {
+            resolve("done");
+        }, 5000)
+    );
+
+    if (!response.ok) {
+        return (
             <div>
-              <div className="bg-gray-200 flex items-center text-sm text-black/50 justify-center h-[200px] rounded border">
-                No image
-              </div>
-              <h2 className="mt-2 flex gap-x-1">
-                <span>{car.Model_Name}</span>
-                <Separator orientation="vertical" />
-              </h2>
-              <p className="text-sm opacity-75 mt-1">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Adipisci, alias aspernatur atque
-              </p>
+                <h1>Failed to fetch data</h1>
+                <p>{result.Message}</p>
             </div>
+        );
+    }
 
-            <div className="flex justify-between w-full items-end">
-              <Button disabled className="mt-4" size="sm">
-                <Info size="16" /> View details
-              </Button>
-              <p className="text-sm">$ 15.999</p>
+    return (
+        <>
+            <h1 className="text-xl">
+                Found {result.Count} {result.Results[0].Make_Name} cars for {year}
+            </h1>
+            <Separator className="mt-4 mb-6"/>
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3">
+                {result.Results.map((car) => (
+                    <div
+                        key={car.Model_ID}
+                        className="p-3 hover:border-black/50 h-[400px] flex-col justify-between transition-colors rounded border"
+                    >
+                        <div>
+                            <div
+                                className="bg-gray-200 flex items-center text-sm text-black/50 justify-center h-[200px] rounded border">
+                                No image
+                            </div>
+                            <h2 className="mt-2 flex gap-x-1">
+                                <span>{car.Model_Name}</span>
+                                <Separator orientation="vertical"/>
+                            </h2>
+                            <p className="text-sm opacity-75 mt-1">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                Adipisci, alias aspernatur atque
+                            </p>
+                        </div>
+
+                        <div className="flex justify-between w-full items-end">
+                            <Button disabled className="mt-4" size="sm">
+                                <Info size="16"/> View details
+                            </Button>
+                            <p className="text-sm">$ 15.999</p>
+                        </div>
+                    </div>
+                ))}
             </div>
-          </div>
-        ))}
-      </div>
-    </>
-  );
+        </>
+    );
 }
--- app/(other)/result/[makeId]/[year]/page.tsx
+++ app/(other)/result/[makeId]/[year]/page.tsx
@@ -1,48 +1,35 @@
-import { carsEndpoint } from "@/lib/constants";
-import { getYearsRangingFrom } from "@/lib/utils";
-import { VehicleDetailsResponse, VehicleListResponse } from "@/lib/types";
-import { Separator } from "@/components/ui/separator";
-import { Button } from "@/components/ui/button";
-import { Info } from "lucide-react";
-import { Suspense } from "react";
+import {carsEndpoint} from "@/lib/constants";
+import {getYearsRangingFrom} from "@/lib/utils";
+import {VehicleListResponse} from "@/lib/types";
+import {Suspense} from "react";
 import Results from "./components/results";
+import {ResultsSkeleton} from "./components/results-skeleton";
 
 export async function generateStaticParams(): Promise<Params[]> {
-  const cars: VehicleListResponse = await fetch(carsEndpoint).then((res) =>
-    res.json()
-  );
-  const years = getYearsRangingFrom(2015);
+    const cars: VehicleListResponse = await fetch(carsEndpoint).then((res) =>
+        res.json()
+    );
+    const years = getYearsRangingFrom(2015);
 
-  return cars.Results.flatMap((car) =>
-    years.map((year) => ({
-      makeId: car.MakeId.toString(),
-      year: year.toString(),
-    }))
-  );
+    return cars.Results.flatMap((car) =>
+        years.map((year) => ({
+            makeId: car.MakeId.toString(),
+            year: year.toString(),
+        }))
+    );
 }
 
 interface Params {
-  makeId: string;
-  year: string;
+    makeId: string;
+    year: string;
 }
 
-export default async function ResultPage({ params }: { params: Params }) {
-  return (
-    <Suspense
-      fallback={
-        <>
-          <h1 className="text-xl animate-pulse">Loading</h1>
-          <Separator className="mt-4 mb-6" />
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3">
-            {Array.from({ length: 8 }).map((_, index) => (
-              <div className="p-3 h-[400px] animate-pulse bg-gray-200 transition-colors rounded border"></div>
-            ))}
-          </div>
-        </>
-      }
-    >
-      <Results params={params} />
-    </Suspense>
-  );
+export default async function ResultPage({params}: { params: Params }) {
+    return (
+        <Suspense
+            fallback={<ResultsSkeleton/>}>
+            <Results params={params}/>
+        </Suspense>
+    );
 }
 
